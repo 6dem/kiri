dc7bb522579e31c9be2411f227790850576e4043
Edem <edem.ablaev57@yandex.ru>
2025-10-16 18:02:08 +0300

    feat: full implementation of Contacts Section without Form

--- src/apps/web/app/(home)/components/Contacts/Contacts.tsx
+++ src/apps/web/app/(home)/components/Contacts/Contacts.tsx
@@ -1,15 +1,6 @@
+import { contacts } from "@data/contacts"
 import Image from "next/image"
 import styles from "./Contacts.module.scss"
-
-const contacts = {
-    email: {link: "mailto:ivanovserezha04@gmail.com", content: "ivanovserezha04@gmail.com", label: "Email"},
-    number: {link: "tel:+79788702341", content: "+7 (978) 870-23-41", label: "Номер телефона"},
-    tg: {link: "https://t.me/schoolgo_kiribot", content: "KiriBot", label: "Телеграм бот"},
-    vk: {link: "https://vk.com/school_go_kiri", content: 'Школа Го "КИРИ"', label: "Сообщество ВКонтакте"},
-    address: {link: "https://yandex.ru/maps/geo/sevastopol/1443782471", content: "г. Севастополь, ул. ---, --", label: "Адрес"}
-}
-
-
 
 export function Contacts() {
   return (
@@ -20,19 +11,22 @@ export function Contacts() {
           <div className={styles.card}>
             <h3>Наши контакты</h3>
             <ul className={styles.contactsList}>
-              {Object.entries(contacts).map(([key, {link, content, label}]) => (
-              <li className={styles.contact} key={key}>
-                <a
-                  className={styles.contactLink}
-                  href={link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label={label}
-                >
-                  <b>{label}</b>: {content}
-                </a>
-              </li>
-            ))}
+              {Object.entries(contacts).map(([key, {link, content, label, icon: Icon}]) => (
+                <li className={styles.contactItem} key={key}>
+                  <div className={styles.contactIcon} title={label}>
+                    {typeof Icon === 'string' ? <img src={Icon} alt={label} width={24} height={24} /> : <Icon size={24} />}
+                  </div>
+                  <a
+                    className={styles.contactLink}
+                    href={link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    aria-label={label}
+                  >
+                    {content}
+                  </a>
+                </li>
+              ))}
               </ul>
           </div>
           <div className={`${styles.card} ${styles.cardLesson}`}>
