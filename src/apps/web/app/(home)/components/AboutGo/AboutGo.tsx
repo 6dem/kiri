1375dc20063b0b678b5e32c2497286824272ac64
Edem <edem.ablaev57@yandex.ru>
2025-12-01 21:07:06 +0300

    feat: web: update about section content

--- src/apps/web/app/(home)/components/AboutGo/AboutGo.tsx
+++ src/apps/web/app/(home)/components/AboutGo/AboutGo.tsx
@@ -14,6 +14,45 @@ export function AboutGo() {
             <p>Го развивает оба полушария мозга, задействуя области как логики и расчёта, так и, интуиции и творчества.</p>
           </div>
         </div>
+        <div className={styles.rulesBlock}>
+          <div className={styles.rulesText}>
+              <blockquote className={styles.quote}>
+                  «Игра Го — самая сложная игра, с самыми простыми правилами»
+              </blockquote>
+              <p className={styles.rulesIntro}>
+                  Мы подготовили для вас удобные материалы.  
+                  В учебнике собраны ключевые основы игры:
+              </p>
+
+              <ul className={styles.rulesList}>
+                  <li>Объяснение правил.</li>
+                  <li>Примеры партий для начинающих.</li>
+                  <li>Этика Го.</li>
+              </ul>
+
+              <p className={styles.rulesFooter}>
+                  Получить учебник и начать обучение можно в нашем Telegram-боте:
+              </p>
+          </div>
+
+          <div className={styles.linkWrapper}>
+              <Image
+                  src="/images/qr-KiriBot.png"
+                  alt="QR код на Telegram-бота"
+                  width={200}
+                  height={200}
+                  className={styles.qr}
+              />
+              <a
+                  href="https://t.me/schoolgo_kiribot"
+                  className={styles.botLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+              >
+                  Открыть бота и получить правила
+              </a>
+          </div>
+        </div>
       </div>
     </section>
 }
