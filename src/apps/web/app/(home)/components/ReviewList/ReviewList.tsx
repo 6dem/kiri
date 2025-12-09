8f473591e6cfffd8df1aca6e9c9ed7a2ec32a7b2
Edem <edem.ablaev57@yandex.ru>
2025-12-09 20:29:09 +0300

    web: style: small fix reviewList icons

--- src/apps/web/app/(home)/components/ReviewList/ReviewList.tsx
+++ src/apps/web/app/(home)/components/ReviewList/ReviewList.tsx
@@ -13,14 +13,14 @@ export function ReviewList() {
   return (
     <section id="reviews" className={styles.ReviewList}>
       <div className="container">
-        <Image className={styles.topRightIcon} src="/icons/top-right-corner.svg" alt="icon" width={200} height={200}/>
+        <Image className={styles.topRightIcon} src="/icons/top-right-corner.svg" alt="icon" width={225} height={225}/>
         <h2 className={styles.stickyTitle}>Что о нас говорят</h2>
         <div className={styles.list}>
           {reviews.slice(0, 3).map((review, idx) => (
             <Review key={idx} {...review} />
           ))}
         </div>
-          <Image className={styles.bottomLeftIcon} src="/icons/bottom-left-corner.svg" alt="icon" width={200} height={200}/>
+          <Image className={styles.bottomLeftIcon} src="/icons/bottom-left-corner.svg" alt="icon" width={225} height={225}/>
       </div>
     </section>
   )
